2a5cd86b14c2b5418f3491a80f0e751e192c352b
underTheBlueSun <wind7129@naver.com>
2021-04-24 15:59:56 +0900

    Initial Commit

---
+++ ANP/ANPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANPApp.swift
+//  ANP
+//
+//  Created by underTheBlueSun on 2021/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ANPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

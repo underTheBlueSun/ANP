0ad393a79c9bf7cd042b9d93d9e4445c07254448
underTheBlueSun <wind7129@naver.com>
2021-05-10 18:45:23 +0900

    2021. 5.10 오후 6:45, 아이맥

--- ANP/Example/Test.swift
+++ ANP/Example/Test.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct Test: View {
     
-    @State private var tip = "1sdfsdfsdfsdfsdafsdfsdsdfsdf동해물과 백두산이 마르고 닳도록 하나님!\nsdfs111dfsdafsdfsdafsda\n"
-    
     var body: some View {
-        VStack(spacing:0){
-            Text("Hello, World!")
-            Text("111111").padding(0)
-            TextEditor(text: $tip).font(.system(size: 13)).foregroundColor(.gray).padding(.vertical, 0)
+        NavigationView {
+            
+            Form {
+                
+                Section {
+                    Text("aaaa")
+                }
+            }
         }
-        
     }
 }
 
